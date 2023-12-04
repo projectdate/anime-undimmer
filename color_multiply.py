16ab7a12c5d8fe1c3625055a6c85a979134cf9b7
Project Date <projectdate.app@gmail.com>
2023-12-04 05:56:33 -0500

    finish undimming the detected dimmed scenes

--- color_multiply.py
+++ color_multiply.py
@@ -3,21 +3,25 @@ from moviepy.editor import VideoFileClip
 import numpy as np
 import matplotlib.pyplot as plt
 
-def multiply_colors(frame, factor):
+def multiply_colors(frame, dimmed_scenes, current_frame):
     """
     Multiply each color in the frame by a given factor.
     Clipping is performed to ensure pixel values stay within valid range.
+    Only multiply colors in the dimmed scenes range.
     """
-    
-    return np.clip(frame * factor, 0, 255).astype('uint8')
+    for start, end, factor in dimmed_scenes:
+        if start <= current_frame < end:
+            return np.clip(frame * factor, 0, 255).astype('uint8')
+    return frame
 
-def process_video(input_file, output_file, factor):
+def process_video(input_file, output_file, dimmed_scenes):
     """
     Process the video, multiplying each frame's pixel values by the specified factor.
+    Only multiply colors in the dimmed scenes range.
     """
     clip = VideoFileClip(input_file)
-    modified_clip = clip.fl_image(lambda frame: multiply_colors(frame, factor))
-    modified_clip.write_videofile(output_file, codec='libx264', audio_codec='aac')
+    clip = clip.fl(lambda gf, t: multiply_colors(gf(t), dimmed_scenes, int(t*clip.fps)))
+    clip.write_videofile(output_file, codec='libx264', audio_codec='aac')
 
 def calculate_epilepsy_risk(frame_values_gen):
     # Initialize variables
@@ -54,9 +58,22 @@ def frame_generator(clip, start, end):
             break
         yield frame
 
-def plot_max_values(input_file, show_plot):
+
+def get_dimmed_scenes(input_file, show_plot):
     """
     Plot max frame value for each half second throughout the video.
+    This function takes in an input video file and a boolean value for whether to show the plot or not.
+    It calculates the maximum frame value for each half second throughout the video and plots it.
+    If show_plot is True, it will display the plot and return an empty list.
+    If show_plot is False, it will not display the plot and will return a list of time ranges where at least 15 consecutive frames have a max below 190.
+    The returned time ranges represent the dimmed scenes in the video.
+    
+    Parameters:
+    input_file (str): The path to the input video file.
+    show_plot (bool): Whether to display the plot or not.
+    
+    Returns:
+    list: A list of time ranges (start frame, end frame, factor) representing the dimmed scenes in the video and how much to undim them by, if show_plot is False. An empty list if show_plot is True.
     """
     clip = VideoFileClip(input_file)
     # Store the max value of frame seen every quarter second
@@ -107,20 +124,20 @@ def plot_max_values(input_file, show_plot):
     for i, value in enumerate(max_values):
         if np.max(value) < 190:
             if count == 0:
-                start_time = i / 24  # Convert frame index to time in seconds
+                start_time = i  # Convert frame index to time in seconds
             count += 1
         else:
             if count >= 15:
-                dark_and_dimmed_ranges.append((start_time, i / 24))  # Add the start and end time of the range
+                dark_and_dimmed_ranges.append((start_time, i))  # Add the start and end time of the range
             count = 0
     if count >= 15:
-        dark_and_dimmed_ranges.append((start_time, len(max_values) / 24))  # Add the last range if it ends at the end of the video
+        dark_and_dimmed_ranges.append((start_time, len(max_values)))  # Add the last range if it ends at the end of the video
     
     # Print characteristics of the values in each range
     dimmed_ranges = []
     for start, end in dark_and_dimmed_ranges:
-        range_values = max_values[int(start*24):int(end*24)]  # Get the values in the range
-        print(f"Time range with at least 15 consecutive values below 193: {int(start/60)}:{start%60:.2f} - {int(end/60)}:{end%60:.2f} minutes")
+        range_values = max_values[int(start):int(end)]  # Get the values in the range
+        print(f"Time range with at least 15 consecutive values below 193: {int((start / 24)/60)}:{(start / 24)%60:.2f} - {int((end / 24)/60)}:{(end / 24)%60:.2f} minutes")
         avg_value = np.mean([np.max(val) for val in range_values])
         max_value = np.max([np.max(val) for val in range_values])
         min_value = np.min([np.min(val) for val in range_values])
@@ -132,12 +149,13 @@ def plot_max_values(input_file, show_plot):
                 print(f"Variance between channels: {np.var(filtered_values, axis=0)}")
         
         # Get the exact frame values in the range using a generator to avoid creating a large temporary list
-        exact_frame_values = frame_generator(clip, start * 24, end * 24)
+        exact_frame_values = frame_generator(clip, start, end)
         risk_mean, risk_stddev = calculate_epilepsy_risk(exact_frame_values)
         print(f"Epileptic risk: {risk_mean:.1f}, {risk_stddev:.1f}")
         if risk_mean > 75 and risk_stddev > 7:
-            print("Likely dimmed scene! Undimming range...")
-            dimmed_ranges.append((start, end))
+            print("Likely dimmed scene! Undimming range: ", (start, end, 256 / max_value))
+            # TODO: Instead of putting 256, put the neighboring scene maxes
+            dimmed_ranges.append((start, end, 256 / max_value))
     
     return dimmed_ranges
 
@@ -150,9 +168,9 @@ def main():
 
     args = parser.parse_args()
 
-    plot_max_values(args.input_file, args.only_plot)
+    dimmed_scenes = get_dimmed_scenes(args.input_file, args.only_plot)
     if (not args.only_plot):
-        process_video(args.input_file, args.output_file, args.factor)
+        process_video(args.input_file, args.output_file, dimmed_scenes)
 
 if __name__ == "__main__":
     main()
