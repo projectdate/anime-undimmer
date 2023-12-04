4de79520f4b0349cc98daf946de4be58ff091587
Project Date <projectdate.app@gmail.com>
2023-12-04 04:34:09 -0500

    calculate epileptic risk and timestamps more accurately

--- color_multiply.py
+++ color_multiply.py
@@ -19,6 +19,21 @@ def process_video(input_file, output_file, factor):
     modified_clip = clip.fl_image(lambda frame: multiply_colors(frame, factor))
     modified_clip.write_videofile(output_file, codec='libx264', audio_codec='aac')
 
+def calculate_epilepsy_risk(frame_values):
+    # Calculate the difference between consecutive frames
+    frame_diffs = np.diff(frame_values, axis=0)
+    # Calculate the absolute sum of pixel differences for each frame difference
+    abs_sum_diffs = np.sum(np.abs(frame_diffs), axis=(1, 2, 3))
+    # Calculate the mean of the absolute sum of differences
+    mean_abs_sum_diff = np.mean(abs_sum_diffs)
+    # Calculate the standard deviation of the absolute sum of differences
+    std_abs_sum_diff = np.std(abs_sum_diffs)
+    # Print the mean and standard deviation of the absolute sum of differences
+    # print(f"Mean absolute sum of differences between consecutive frames: {mean_abs_sum_diff}")
+    print(f"Standard deviation of absolute sum of differences between consecutive frames: {std_abs_sum_diff}")
+    # If the standard deviation is high, the video is more likely to cause epilepsy
+    return std_abs_sum_diff
+
 def plot_max_values(input_file):
     """
     Plot max frame value for each half second throughout the video.
@@ -27,6 +42,7 @@ def plot_max_values(input_file):
     # Store the max value of frame seen every quarter second
     print("Starting analysis...")
     max_values = []
+    max_values_per_6_frames = []
     import os
     import pickle
     
@@ -35,42 +51,63 @@ def plot_max_values(input_file):
     
     # Check if the cache file exists
     if os.path.exists(cache_file):
+        print("Cached!")
         # Load the max values from the cache file
         with open(cache_file, 'rb') as f:
             max_values = pickle.load(f)
     else:
+        print("Not cached!")
         # Calculate the max values and store them in the cache file
         for i, (t, frame) in enumerate(clip.iter_frames(with_times=True)):
-            if i % 6 == 0:  # Check every 1/4 of a second, so every 6th frame
-                max_values.append(np.max(frame))
+            max_frame = np.max(frame, axis=(0, 1))
+            max_values.append(max_frame)
         with open(cache_file, 'wb') as f:
             pickle.dump(max_values, f)
-            
-    print(max_values)
-    plt.plot([x/4 for x in range(len(max_values))], max_values)
-    plt.title('Max frame value per quarter second')
-    plt.xlim(0, len(max_values)/4)  # Set x-axis range to match the number of data points in seconds
-    plt.xticks(np.arange(0, len(max_values)/4, 5))  # Show ticks on the x axis every 5 seconds
     
-    # Find all the time ranges where at least 3 consecutive values are below 180
-    below_180_ranges = []
+    # Calculate max_values_per_6_frames from every 6 frames in max_values
+    for i in range(0, len(max_values), 6):
+        frames_to_average = max_values[i:i+6]
+        max_frame = np.max(frames_to_average, axis=0)
+        max_values_per_6_frames.append(max_frame)
+        
+    print("Max every 6 frames", max_values_per_6_frames)
+    plt.plot([x/4 for x in range(len(max_values_per_6_frames))], [np.max(val) for val in max_values_per_6_frames])
+    plt.title('Max frame value per quarter second')
+    plt.xlim(0, len(max_values_per_6_frames)/4)  # Set x-axis range to match the number of data points in seconds
+    
+    # Find all the time ranges where at least 15 consecutive frames have a max below 190
+    dimmed_ranges = []
     count = 0
     start_time = 0
     for i, value in enumerate(max_values):
-        if value < 180:
+        if np.max(value) < 190:
             if count == 0:
-                start_time = i / 4  # Convert frame index to time in seconds
+                start_time = i / 24  # Convert frame index to time in seconds
             count += 1
         else:
-            if count >= 3:
-                below_180_ranges.append((start_time, i / 4))  # Add the start and end time of the range
+            if count >= 15:
+                dimmed_ranges.append((start_time, i / 24))  # Add the start and end time of the range
             count = 0
-    if count >= 3:
-        below_180_ranges.append((start_time, len(max_values) / 4))  # Add the last range if it ends at the end of the video
+    if count >= 15:
+        dimmed_ranges.append((start_time, len(max_values) / 24))  # Add the last range if it ends at the end of the video
     
-    # Print all the time ranges where at least 3 consecutive values are below 180
-    for start, end in below_180_ranges:
-        print(f"Time range with at least 3 consecutive values below 180: {start} - {end} seconds")
+    # Print characteristics of the values in each range
+    for start, end in dimmed_ranges:
+        range_values = max_values[int(start*24):int(end*24)]  # Get the values in the range
+        print(f"Time range with at least 3 consecutive values below 180: {int(start/60)}:{start%60:.2f} - {int(end/60)}:{end%60:.2f} minutes")
+        print(f"Average value: {np.mean([np.max(val) for val in range_values])}")
+        print(f"Max value: {np.max([np.max(val) for val in range_values])}")
+        print(f"Min value: {np.min([np.min(val) for val in range_values])}")
+        print(f"Variance: {np.var([np.var(val) for val in range_values])}")
+        if len(range_values) > 0:
+            filtered_values = [x for x in range_values if isinstance(x, np.ndarray) and x.shape == (3,)]
+            if len(filtered_values) > 0:
+                print(f"Variance between channels: {np.var(filtered_values, axis=0)}")
+        
+        range_values = [np.max(frame, axis=(0, 1)) for frame in clip.iter_frames()][int(start*24):int(end*24)]  # Get the values in the range
+        risk = calculate_epilepsy_risk(range_values)
+        print(f"Epileptic risk: ", risk)
+        
     plt.show()
 
 def main():
